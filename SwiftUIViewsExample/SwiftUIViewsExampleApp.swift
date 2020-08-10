e75e10668b811c5280e6afec4c7cb1d534a42b37
Ankush Bhatia <ankushbhatia1347@gmail.com>
2020-08-11 01:44:16 +0530

    Added SwiftUI project for initial setup.

---
+++ SwiftUIViewsExample/SwiftUIViewsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsExampleApp.swift
+//  SwiftUIViewsExample
+//
+//  Created by Ankush Bhatia on 11/08/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
